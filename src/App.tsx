b71294ef73b38883f832dfe9cfe51a1ee3e97b48
Dean Kinyua <kinyuadean@gmail.com>
2024-06-30 14:50:42 +0300

    shadcn works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import { Button } from "../src/components/ui/button";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../src/components/ui/card";
+
 const App = () => {
   return (
     <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+
       <Button>Click me</Button>
     </div>
   );
